7dc01c18abaec7fb46f9fc1e87b649a367ae6296
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-16 02:37:09 +0300

    debugging to ensure it catches top titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,32 +3,27 @@
 """
 Display the top 10 hot posts in a given reddit
 """
-
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
-
     """
     Function that queries the Reddit API and prints the titles of the
     first 10 hot posts listed for a given subreddit.
     """
-
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
-
-    headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    headers = {
+            'User-Agent': 'my-api-agent'
+            }
+    params = {
+            'limit': 10
+            }
+    response = get(url, headers=headers, params=params, allow_redirects=False)
 
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()
-
-    try:
-        my_data = data.get('data').get('children')
-
-        for j in my_data:
-            print(j.get('data').get('title'))
-
-    except Exception:
+    if response.status_code == 404:
         print("None")
+        return
+    else:
+        data = response.json().get('data')
+
+        [print(i.get('data').get('title')) for i in data.get('children')]
