7dff5a2b8b9fa9c595b5954cf0181b081a061765
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-21 05:03:07 +0300

    Remove file limit restriction on user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Remove restriction of open files on user holberton
+
+exec { 'remove hard limit':
+command => 'sed -i "56" /etc/security/limits.conf',
+path    => '/bin/'
+}
+
+-> exec { 'remove soft limit':
+command => 'sed -i "57d" /etc/security/limits.conf',
+path    => '/bin/'
+}
