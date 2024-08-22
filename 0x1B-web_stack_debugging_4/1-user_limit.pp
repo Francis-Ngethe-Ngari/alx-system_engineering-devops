d6781a6da4ec8b171744510f2f7165d276c4cb5c
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-22 23:50:57 +0300

    fixing missingg char

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 # Remove restriction of open files on user holberton
 
 exec { 'remove hard limit':
-command => 'sed -i "56" /etc/security/limits.conf',
+command => 'sed -i "56d" /etc/security/limits.conf',
 path    => '/bin/'
 }
 
