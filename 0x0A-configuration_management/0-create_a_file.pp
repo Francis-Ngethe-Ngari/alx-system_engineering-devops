5071bf0dda1e289e5bc374c0b61e3365718dc227
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-23 13:02:34 +0300

    Use puppet to automate operations e.g create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,21 @@
+# Creating a file in /tmp/ dir
+
+exec{ 'creating a file in tmp dir and add text':
+command => 'touch /tmp/school',
+path    => '/bin/'
+}
+
+-> exec { 'change file permission':
+command => 'chmod 0744 /tmp/school',
+path    => '/bin/'
+}
+
+-> exec { 'change file owner and group':
+command => 'chwon -R www-data:www-data /tmp/school',
+path    => '/bin/'
+}
+
+-> exec { 'Add text':
+command => 'echo "I love Puppet" /tmp/school',
+path    => '/bin/'
+}
