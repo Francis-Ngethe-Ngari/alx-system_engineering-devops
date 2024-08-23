978c538bd7676cce8ce28102ca680f05cccd3b24
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-23 13:40:29 +0300

    re-wrote syntax

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,26 +1,8 @@
 # Creating a file in /tmp/ dir
 
-exec{ 'creating a file in tmp dir and add text':
-command => 'touch /tmp/school',
-path    => '/bin/'
-}
-
--> exec { 'change file permission':
-command => 'chmod 0744 /tmp/school',
-path    => '/bin/'
-}
-
--> exec { 'change file owner':
-command => 'chwon www-data /tmp/school',
-path    => '/bin/'
-}
-
--> exec { 'change file group':
-command => 'chwon :www-data /tmp/school',
-path    => '/bin/'
-}
-
--> exec { 'Add text':
-command => 'echo "I love Puppet" > /tmp/school',
-path    => '/bin/'
+file { '/tmp/school':
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
