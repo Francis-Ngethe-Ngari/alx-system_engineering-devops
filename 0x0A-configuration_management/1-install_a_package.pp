2b05e4895af0a43e09b3fe6f213163573c05e94f
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-24 06:15:21 +0300

    installing a compatile werkzaug dependancy

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,8 @@ exec { 'install flask pakage':
   command => 'pip install Flask==2.1.0',
   path    => '/bin/',
 }
+
+-> exec { 'install werkzaug pakage':
+    command => 'pip install werkzeug==2.1.1',
+    path    => '/bin/',
+}
