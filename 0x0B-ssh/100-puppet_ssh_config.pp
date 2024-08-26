21cc8987f01c0b87548ef0a54e03f09854fd5741
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-26 13:19:49 +0300

    debugging

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,15 @@
 # Configure SSH client to use Private Key and refuse password
 # authentication.
 
+package { 'openssh-clinet':
+ensure => installed,
+}
+
 file { 'etc/ssh/sshd_config':
 ensure  => file,
 mode    => '0644',
 owner   => 'root',
 group   => 'root',
-content => '
-Host *
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-',
+content => '/etc/ssh/sshd_config.erb',
+require => package['openssh-client'],
 }
