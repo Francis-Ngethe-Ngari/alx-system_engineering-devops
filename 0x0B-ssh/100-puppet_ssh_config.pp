8ebc4354f0abd5954d13b010e1d9365ffadc1c86
Francis-Ngethe-Ngari <francisngari49@gmail.com>
2024-08-26 14:56:39 +0300

    debugging trial4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,14 +7,13 @@ ensure => 'installed',
 
 file_line { 'Disable password auth':
 path    => '/etc/ssh/ssh_config',
-line    => ' PasswordAuthentication no',
-match   => '^.*PasswordAuthentication no.*'
-require => package['openssh-client'],
+line    => 'PasswordAuthentication no',
+match   => 'PasswordAuthentication yes',
+replace => true
 }
 
 file_line { 'Use private key':
-path    => '/etc/ssh/ssh_config',
-line    => ' IdentityFile ~/.ssh/school',
-match   => '^.*IdentityFile.*',
-require => package['openssh-client'],
+path  => '/etc/ssh/ssh_config',
+line  => 'IdentityFile ~/.ssh/school',
+match => '^IdentityFile',
 }
